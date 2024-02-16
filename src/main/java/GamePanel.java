9b0c3f218b02fde17f5ab88330ec5c0b1d5934a5
Medrea Sergiu <msergiu2015@gmail.com>
2024-02-16 18:07:58 +0200

    The apples can no longer spawn inside the snake

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -74,8 +74,19 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void newApple() {
-        appleX = random.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
-        appleY = random.nextInt(SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+        boolean conflict;
+        do {
+            conflict = false;
+            appleX = random.nextInt((SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+            appleY = random.nextInt((SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+
+            for (int i = 0; i < bodyParts; i++) {
+                if (x[i] == appleX && y[i] == appleY) {
+                    conflict = true;
+                    break;
+                }
+            }
+        } while (conflict);
     }
 
     public void move() {
